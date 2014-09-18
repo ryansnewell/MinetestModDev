0a4e373aa3be6b501526e05c92407ea7cc09a846
Ryan Newell <thekingdoof@gmail.com>
2014-09-18 17:31:13 -0500

    Computer block fixes

--- init.lua
+++ init.lua
@@ -1,4 +1,21 @@
-minetest.register_node("tutorial:computer", {
+minetest.register_node("testmod:computer", {
 	tiles = {"computer-block.png"},
-	groups = {cracky=2,snappy=2,choppy=2,metal=3},
+	groups = {cracky=3},
+})
+
+minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing) 
+	if newnode.name == "tutorial:computer" then
+		minetest.chat_send_all("You placed a computer")
+	else
+		minetest.chat_send_all("Tsk tsk")
+	end
+end)
+
+minetest.register_craft({
+	output = "testmod:computer 3",
+	recipe = {
+			{'default:wood', 'default:wood', ''},
+			{'default:wood', 'default:wood', ''},
+			{'', '', ''},
+	}
 })
