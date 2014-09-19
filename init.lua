3a27f0d79b37ead62add1a106596d817bbd5e686
Ryan Newell <thekingdoof@gmail.com>
2014-09-19 17:07:38 -0500

    Added hud, doesnt work though

--- init.lua
+++ init.lua
@@ -19,3 +19,17 @@ minetest.register_craft({
 			{'', '', ''},
 	}
 })
+
+minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
+	if node.name == "testmod:computer" then
+		--minetest.chat_send_all("You punched me, you cunt")
+		formspec = [[
+			size[8,9;]
+			list[context;fuel;1,1;1,1;]
+			list[context;src;2,2;1,1;]
+			list[context;dst;5,1;2,2;]
+			list[current_player;main;0,5;8,4;]
+		]]
+		minetest.show_formspec(puncher:get_player_name(), "testmod:computer-field", formspec)
+	end
+end)
