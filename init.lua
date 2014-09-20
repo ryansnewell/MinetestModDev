8dd25089e65142b620da7ff6d1cb0572e93ec29b
Ryan Newell <thekingdoof@gmail.com>
2014-09-19 22:40:08 -0500

    Added comments

--- init.lua
+++ init.lua
@@ -1,16 +1,20 @@
+
+--Register computer node
 minetest.register_node("testmod:computer", {
 	tiles = {"computer-block.png"},
 	groups = {cracky=3},
 })
 
+--Just testing
 minetest.register_on_placenode(function(pos, newnode, placer, oldnode, itemstack, pointed_thing) 
-	if newnode.name == "tutorial:computer" then
+	if newnode.name == "testmod:computer" then
 		minetest.chat_send_all("You placed a computer")
 	else
 		minetest.chat_send_all("Tsk tsk")
 	end
 end)
 
+--Register computer recipe
 minetest.register_craft({
 	output = "testmod:computer 3",
 	recipe = {
@@ -20,14 +24,13 @@ minetest.register_craft({
 	}
 })
 
+--Register formspec for computer node
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	if node.name == "testmod:computer" then
 		--minetest.chat_send_all("You punched me, you cunt")
 		formspec = [[
-			size[8,9;]
-			list[context;fuel;1,1;1,1;]
-			list[context;src;2,2;1,1;]
-			list[context;dst;5,1;2,2;]
+			invsize[8,9;]
+			list[context;item;2,2;1,1;]
 			list[current_player;main;0,5;8,4;]
 		]]
 		minetest.show_formspec(puncher:get_player_name(), "testmod:computer-field", formspec)
