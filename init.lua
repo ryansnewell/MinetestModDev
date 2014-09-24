262abc74b22335641a093246d30724c99d8d434e
Ryan Newell <thekingdoof@gmail.com>
2014-09-23 20:16:45 -0500

    Updated the Computer node GUI

--- init.lua
+++ init.lua
@@ -3,6 +3,19 @@
 minetest.register_node("testmod:computer", {
 	tiles = {"computer-block.png"},
 	groups = {cracky=3},
+	after_place_node = function(pos, placer)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("infotext", "Computer owned by " .. placer:get_player_name())
+	end,
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("formspec", "invsize[8,9;] list[context;fuel;2,1;1,1;] list[context;src;2,3;1,1;] list[context;dst;5,2;1,1;] list[current_player;main;0,5;8,4;]")
+		meta:set_string("infotext", "Computer")
+		local inv = meta:get_inventory()
+		inv:set_size("fuel", 1)
+		inv:set_size("src", 1)
+		inv:set_size("dst", 6)
+	end,
 })
 
 --Just testing
@@ -27,11 +40,13 @@ minetest.register_craft({
 --Register formspec for computer node
 minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	if node.name == "testmod:computer" then
-		formspec = [[
-			invsize[8,9;]
-			list[context;item;2,2;1,1;]
-			list[current_player;main;0,5;8,4;]
-		]]
-		minetest.show_formspec(puncher:get_player_name(), "testmod:computer-field", formspec)
+		--formspec = [[
+		--	invsize[8,9;]
+		--	list[context;item;2,2;1,1;]
+		--	list[current_player;main;0,5;8,4;]
+		--]]
+		--minetest.show_formspec(puncher:get_player_name(), "testmod:computer-field", formspec)
+		
+		
 	end
 end)
